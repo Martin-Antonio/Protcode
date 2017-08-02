45f5ed5251f1648403e3ac9428de22241e22dc51
martin  antonio tirado mercedes <jhv23corp@gmail.com>
2017-08-02 19:11:04 -0400

    heroku deploy1

--- Grapes/settings.py
+++ Grapes/settings.py
@@ -78,20 +78,20 @@ WSGI_APPLICATION = 'Grapes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}"""
- DATABASES = {
+}
+ """DATABASES = {
       'default' : {
          'ENGINE' : 'django.db.backends.postgresql_psycopg2',
          'NAME' : 'db_name',
          #setear USERNAME Y PASS si necesitan.
       }
   } 
-
+"""
 
 
 # Password validation
