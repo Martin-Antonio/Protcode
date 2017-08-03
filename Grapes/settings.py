5d79da7407be6e803cedba9e587b9a90d98070c0
martin  antonio tirado mercedes <jhv23corp@gmail.com>
2017-08-03 12:12:04 -0400

    'g'

--- Grapes/settings.py
+++ Grapes/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'Grapes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(SETTINGS_PATH, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
