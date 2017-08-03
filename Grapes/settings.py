d04659f76bd41d1b7dea6ea0ebc8001cab4c021a
martin  antonio tirado mercedes <jhv23corp@gmail.com>
2017-08-03 00:31:05 -0400

    'deploy4'

--- Grapes/settings.py
+++ Grapes/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'Grapes.wsgi.application'
 
 DATABASES = {
     'default': {
-       # 'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',# Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'd6ghqpucmnv14c',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'measltnihekukq',
