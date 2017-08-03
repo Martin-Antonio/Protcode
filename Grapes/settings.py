dae98f644938ed2a3ab51889e8167a2d798ee486
martin  antonio tirado mercedes <jhv23corp@gmail.com>
2017-08-02 20:12:16 -0400

    heroku deploy3

--- Grapes/settings.py
+++ Grapes/settings.py
@@ -78,13 +78,13 @@ WSGI_APPLICATION = 'Grapes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+"""
 if bool(os.environ.get('LOCAL_DEV', False)): 
   DATABASES = {
       'default' : {
@@ -97,11 +97,11 @@ else:
   DATABASES = {
       'default' : dj_database_url.config(default='postrgres://localhost')
   }
-"""
+
 
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
