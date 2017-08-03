bc081bb21a778cac33c1f65544a0d8578a783329
martin  antonio tirado mercedes <jhv23corp@gmail.com>
2017-08-03 12:27:58 -0400

    'g'

--- Grapes/settings.py
+++ Grapes/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import dj_database_url
-SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -38,11 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-     'App.admin_app',
-     'App.Developmet_group',
-     'App.blog',
-     'ckeditor',
-     'gunicorn',
+    'App.admin_app',
+    'App.Developmet_group',
+    'App.blog',
+    'ckeditor',
+    'gunicorn'
 ]
 
 MIDDLEWARE = [
